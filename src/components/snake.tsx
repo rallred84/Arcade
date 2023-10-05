17cd30d7b6748857f389ab8704648443465aee22
Robert Allred <rallred84@gmail.com>
2023-10-04 21:02:48 -0500

    Added initial placement of snake

--- src/components/snake.tsx
+++ src/components/snake.tsx
@@ -5,9 +5,17 @@ import { useEffect, useState } from "react";
 const Snake = () => {
   const { user } = useOutletContext<ContextType>();
   const [rows, setRows] = useState<string[]>([]);
-  const [rowCount, setRowCount] = useState(10);
+  const [rowCount, setRowCount] = useState(15);
   const [columns, setColumns] = useState<string[]>([]);
-  const [columnCount, setColumnCount] = useState(12);
+  const [columnCount, setColumnCount] = useState(15);
+
+  type Snake = {
+    head: string;
+    body: string[];
+    tail: string;
+  };
+
+  const [snake, setSnake] = useState<Snake>({ head: "", body: [""], tail: "" });
 
   useEffect(() => {
     if (rows.length !== rowCount) {
@@ -26,15 +34,30 @@ const Snake = () => {
       console.log(columnArray);
       setColumns(columnArray);
     }
-  }, [rowCount, rows, columnCount, columns]);
+
+    setSnake({
+      head: `Row ${Math.ceil(rowCount / 2)},Column ${Math.ceil(
+        columnCount / 2
+      )}`,
+      body: [
+        `Row ${Math.ceil(rowCount / 2)},Column ${Math.ceil(
+          columnCount / 2 - 1
+        )}`,
+      ],
+      tail: `Row ${Math.ceil(rowCount / 2)},Column ${Math.ceil(
+        columnCount / 2 - 2
+      )}`,
+    });
+    console.log(snake);
+  }, [rowCount, rows, columnCount, columns, snake]);
 
   return (
     <>
       <h2>Welcome to Snake, {user.username}!</h2>
       <select name="rows" onChange={(e) => setRowCount(Number(e.target.value))}>
         <optgroup label="Choose Rows">
-          <option value={10}>10</option>
           <option value={15}>15</option>
+          <option value={25}>25</option>
         </optgroup>
       </select>
       <select
@@ -42,8 +65,8 @@ const Snake = () => {
         onChange={(e) => setColumnCount(Number(e.target.value))}
       >
         <optgroup label="Choose Columns">
-          <option value={10}>10</option>
           <option value={15}>15</option>
+          <option value={25}>25</option>
         </optgroup>
       </select>
       {rows.map((row, i) => {
@@ -52,7 +75,7 @@ const Snake = () => {
             {columns.map((column, k) => (
               <div
                 key={k}
-                data-cell-id={`${row}, ${column}`}
+                data-cell-id={`${row},${column}`}
                 className={`cell ${
                   i % 2 === 0
                     ? k % 2 === 0
@@ -61,6 +84,11 @@ const Snake = () => {
                     : k % 2 === 0
                     ? "blue"
                     : "yellow"
+                } ${
+                  (snake.head === `${row},${column}` ||
+                    snake.body.includes(`${row},${column}`) ||
+                    snake.tail === `${row},${column}`) &&
+                  "red"
                 }`}
               ></div>
             ))}
